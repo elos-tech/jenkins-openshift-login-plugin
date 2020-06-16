b1b3c0e6caaaccf31d014baecea1b07a27abbe48
Lukas Smiga <lukas.smiga@elostech.cz>
2020-06-17 00:02:39 +0200

    dtto

--- src/main/java/org/openshift/jenkins/plugins/openshiftlogin/OpenShiftGroupInfo.java
+++ src/main/java/org/openshift/jenkins/plugins/openshiftlogin/OpenShiftGroupInfo.java
@@ -31,7 +31,7 @@ import com.google.api.client.util.Key;
 
 public class OpenShiftGroupInfo extends GenericJson {
 
-    @Key("name")
+    @Key("metadata.name")
     public String name;
 
     @Key("users")
